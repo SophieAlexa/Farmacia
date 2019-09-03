062a595a1e4094c56a9f4104e6de8b4fdfb6e732
Sofia Aquino <49851422+SophieAlexa@users.noreply.github.com>
2019-09-02 21:00:18 -0600

    Create DaoVentas.java

---
+++ Login/ProyectoFarmacia/src/sistemax/DAO/DaoVentas.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemax.DAO;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import sistemax.Identidades.VentasCliente;
+
+/**
+ *
+ * @author Admin
+ */
+public class DaoVentas extends Conexion{
+    
+    PreparedStatement ps;
+    ResultSet rs;
+    VentasCliente vc;
+    int res;
+    
+}
